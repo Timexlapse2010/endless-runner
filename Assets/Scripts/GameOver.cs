2ca3eb9bda8f5c85314ebfe2155575da297a536d
Raees <raeeskaizo@gmail.com>
2025-11-05 19:59:04 +0800

    scoretext

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -19,6 +19,6 @@ public class GameOver : MonoBehaviour
     public void Restart()
     {
        // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-       SceneManager.LoadScene("SampleScene");
+       //SceneManager.LoadScene("SampleScene");
     }
 }
